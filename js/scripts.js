d4f9ea4735a6cd6d268c905ddadd321034829ab4
Brandon Martin <brandon.martin101@gmail.com>
2017-01-20 14:30:47 -0700

    Uploading files

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+
+    var viewportHeight = $(window).height();
+    $("header").height(viewportHeight);
+    $(".name").css("padding-top",viewportHeight*0.1);
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
